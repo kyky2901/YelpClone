f27a2acad16a77451acdb78e0b81c0f3d364ce70
Kylie <kylie.rae.slattery@gmail.com>
2019-12-14 00:56:19 +0000

    added google api

---
+++ app/models/alter_places_add_lat_and_lng.rb
@@ -0,0 +1,2 @@
+class AlterPlacesAddLatAndLng < ApplicationRecord
+end
---
+++ test/models/alter_places_add_lat_and_lng_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AlterPlacesAddLatAndLngTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
